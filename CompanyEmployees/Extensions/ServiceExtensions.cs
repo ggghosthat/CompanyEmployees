ab02e6f4f10687070c56a51776a4595114b20f50
ggghosthat <ildarildar990@gmail.com>
2022-08-09 00:18:35 +0300

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Cors Policy", builder => 
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+                options.AutomaticAuthentication = true;
+                options.AuthenticationDisplayName = "Ollarius";
+            });
+        }
+    }
+}
